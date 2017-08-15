d0f1c4e70dbb51c32ad32df1235e6cd151384e99
Alexey Gribovskiy <agribovskiy@tsf-484-wpa-5-149.epfl.ch>
2017-08-15 15:32:37 +0200

    Apply some editing and bugfixing.

--- get-board-data.py
+++ get-board-data.py
@@ -7,10 +7,10 @@ def filter_data(data, directions):
         for p in data['stationboard']:
             line_name = p['name']
             direction = p['to']
-            if (direction in directions):
-                if (line_name in sorted_data):
-                    if (direction in sorted_data[line_name]):
-                        if (len(p['passList'])>0):
+            if direction in directions:
+                if line_name in sorted_data:
+                    if direction in sorted_data[line_name]:
+                        if len(p['passList']) > 0:
                             sorted_data[line_name][direction].append(p['passList'][0]['arrival'])
                     else:
                         sorted_data[line_name][direction] = list()
@@ -30,14 +30,11 @@ def filter_data(data, directions):
 if __name__ == '__main__':
     with open('data/data.json') as json_file:  
         data = json.load(json_file)
-        for p in data['stationboard']:
-            print('category: ' + p['category'])
-            print('name: ' + p['name'])
-            print('to: ' + p['to'])
-            if (len(p['passList'])>0):
-                print('arrival: ' + p['passList'][0]['arrival'])
-            print('')
-
+        directions = {'Lausanne-Flon', 'Renens VD, gare'}
+        sorted_data = filter_data(data, directions)
+        for line in sorted_data:
+            for direction in sorted_data[line]:
+                print(line + ' to ' + direction + ':' + ' '.join(sorted_data[line][direction]))
     
     #url = "http://transport.opendata.ch/v1/stationboard?station=Lausanne,Malley&limit=10"
     #response = requests.get(url)
